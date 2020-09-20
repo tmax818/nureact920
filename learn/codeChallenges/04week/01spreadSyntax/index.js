ed2938e0fc6ab3ff8c9e40c0a470496d2d991911
tmax818 <tylermaxwell661@gmail.com>
2020-09-20 12:22:11 -0700

    start

---
+++ learn/codeChallenges/04week/01spreadSyntax/index.js
@@ -0,0 +1,29 @@
+/* Challenge 1 - spread the two arrays below to create a new array 'philosophers' */
+const ancientGreekPhilosophers = ["Hypatia", "Socrates", "Zeno"];
+const modernPhilosophers = ["Allan Gibbard", "Susan Haack", "Judith Butler"];
+
+const philosophers = [
+  /* your code here */
+];
+console.log(philosophers);
+
+/* Challenge 2 - spread the array below to create a new array 'newStudents' that includes the given studentToAdd object */
+const STUDENTS = [{ name: "Rocky" }, { name: "Celine" }, { name: "Masood" }];
+const studentToAdd = { name: "Krystal" };
+
+const newStudents = [
+  /* your code here */
+];
+console.log(newStudents);
+
+/* Challenge 3 - spread the object below to create a new object that updates the students property with the newStudents array that you created in Challenge 2 */
+const reactCourse = {
+  durationInWeeks: 5,
+  name: "React",
+  students: STUDENTS,
+};
+
+const reactCourseUpdated = {
+  /* your code here */
+};
+console.log(reactCourseUpdated);
