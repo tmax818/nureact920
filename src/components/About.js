5055dcbe9e56e7f7f78a06adac71a39e75c856d5
tmax818 <tylermaxwell661@gmail.com>
2020-10-04 14:33:33 -0700

    finish

--- src/components/About.js
+++ src/components/About.js
@@ -8,8 +8,34 @@ import {
   Media,
 } from "reactstrap";
 import { Link } from "react-router-dom";
-import Loading from "./LoadingComponent";
+import { Loading } from "./LoadingComponent";
 import { baseUrl } from "../shared/baseUrl";
+
+const PartnerList = (props) => {
+  console.log(props);
+  const partners = props.partners.partners.map((partner) => {
+    return (
+      <Media tag="li" key={partner.id}>
+        <RenderPartner partner={partner} />
+      </Media>
+    );
+  });
+  if (props.partners.isLoading) {
+    return (
+      <div className="col">
+        <Loading />
+      </div>
+    );
+  }
+  if (props.partners.errMess) {
+    return <h4>{(props.partners.isLoading, props.partners.errMess)}</h4>;
+  }
+  return (
+    <div className="col mt-4">
+      <Media list>{partners}</Media>
+    </div>
+  );
+};
 
 function RenderPartner({ partner }) {
   if (partner) {
@@ -32,14 +58,6 @@ function RenderPartner({ partner }) {
 }
 
 function About(props) {
-  const partners = props.partners.partners.map((partner) => {
-    return (
-      <Media tag="li" key={partner.id}>
-        <RenderPartner partner={partner} />
-      </Media>
-    );
-  });
-
   return (
     <div className="container">
       <div className="row">
@@ -109,9 +127,10 @@ function About(props) {
         <div className="col-12">
           <h3>Community Partners</h3>
         </div>
-        <div className="col mt-4">
+        {/* <div className="col mt-4">
           <Media list>{partners}</Media>
-        </div>
+        </div> */}
+        <PartnerList partners={props.partners} />
       </div>
     </div>
   );
