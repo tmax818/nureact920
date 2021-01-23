54e65f05d1430773ff4c94a5f161c614b1911c1f
tmax818 <tylermaxwell661@gmail.com>
2021-01-22 18:56:14 -0800

    clean file

--- learn/workshops/01week/lecture/advArray.js
+++ learn/workshops/01week/lecture/advArray.js
@@ -5,10 +5,6 @@
 
 // const students = ["James", "Jose", "Katya", "Kelsey", "Sergey"];
 
-// const kstudents = students.filter(name => name[0] !== "S")
-
-// console.log(kstudents);
-
 // Map
 
 // takes a cb
@@ -16,18 +12,7 @@
 
 
 
-// const dnumbers = numbers.map(number => number * 2)
 
-// console.log(dnumbers)
-
-
-
-
-
-
-// const squared = numbers.map((number) => number ** 2);
-
-// console.log(squared);
 
 // Reduce
 
@@ -36,18 +21,3 @@
 // the reducer takes two args: an accumulator and a current value
 const numbers = [1, 2, 3];
 
-const reduceNumbers = numbers.reduce((acc, cur) => {
-  console.log("acc:", acc,"cur", cur)
-  return acc + cur
-
-}, 0)
-
-console.log(reduceNumbers)
-
-// const sum = numbers.reduce((acc, cur, i, arr) => {
-//   arr.push(cur);
-//   console.log(`acc: ${acc}, cur: ${cur}, i: ${i}, arr: ${arr}`);
-//   return acc + cur;
-// }, 0);
-
-// console.log(sum);
