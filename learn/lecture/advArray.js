76c15df8050a088bee363c86530b88d1827e52a9
tmax818 <tylermaxwell661@gmail.com>
2021-02-27 08:40:28 -0800

    update file struct

--- learn/lecture/advArray.js
+++ learn/lecture/advArray.js
@@ -34,15 +34,15 @@
 // takes a cb, "the reducer" and an initial value
 // returns a single output value
 // the reducer takes two args: an accumulator and a current value
-const numbers = [1, 2, 3];
+// const numbers = [1, 2, 3];
 
-const reduceNumbers = numbers.reduce((acc, cur) => {
-  console.log("acc:", acc,"cur", cur)
-  return acc + cur
+// const reduceNumbers = numbers.reduce((acc, cur) => {
+//   console.log("acc:", acc,"cur", cur)
+//   return acc + cur
 
-}, 0)
+// }, 0)
 
-console.log(reduceNumbers)
+// console.log(reduceNumbers)
 
 // const sum = numbers.reduce((acc, cur, i, arr) => {
 //   arr.push(cur);
